afbe81169afb9d2585f980475a63bc31b52d0d96
K <129178072+xiasi0@users.noreply.github.com>
2026-09-22 20:44:22 +0800

    chore: remove unused entity category support

--- custom_components/huawei_smarthome/device_adapters/api.py
+++ custom_components/huawei_smarthome/device_adapters/api.py
@@ -32,10 +32,6 @@ class EntitySpec:
     event_decoder: EventDecoder | None = None
     availability: AvailabilityReader | None = None
 
-    def __post_init__(self):
-        if self.metadata is None:
-            self.metadata = {}
-
 
 class HuaweiProductAdapter(Protocol):
     """Interface implemented by one ``prod_<prodId>.py`` file."""
